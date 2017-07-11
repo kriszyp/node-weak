fbf94ad9ea5e347c09534b840e59ad7664dcab34
Kris Zyp <kriszyp@gmail.com>
2017-07-11 12:32:25 -0600

    Strip out all callbacks to see if it improves stability

--- src/weakref.cc
+++ src/weakref.cc
@@ -25,7 +25,6 @@ namespace {
 class proxy_container {
 public:
   Nan::Persistent<Object> proxy;
-  Nan::Persistent<Object> emitter;
   Nan::Persistent<Object> target;
 };
 
@@ -53,109 +52,29 @@ Local<Object> Unwrap(Local<Object> proxy) {
   return _target;
 }
 
-Local<Object> GetEmitter(Local<Object> proxy) {
-  proxy_container *cont = reinterpret_cast<proxy_container*>(
-    Nan::GetInternalFieldPointer(proxy, 0)
-  );
-  assert(cont != NULL);
-  Local<Object> _emitter = Nan::New<Object>(cont->emitter);
-  return _emitter;
-}
-
 
 #define UNWRAP                            \
   Local<Object> obj;                      \
   const bool dead = IsDead(info.This());  \
   if (!dead) obj = Unwrap(info.This());   \
 
-
-NAN_PROPERTY_GETTER(WeakNamedPropertyGetter) {
-  UNWRAP
-  info.GetReturnValue().Set(dead ? Local<Value>() : Nan::Get(obj, property).ToLocalChecked());
-}
-
-
-NAN_PROPERTY_SETTER(WeakNamedPropertySetter) {
-  UNWRAP
-  if (!dead) Nan::Set(obj, property, value);
-  info.GetReturnValue().Set(value);
-}
-
-
-NAN_PROPERTY_QUERY(WeakNamedPropertyQuery) {
-  info.GetReturnValue().Set(None);
-}
-
-
-NAN_PROPERTY_DELETER(WeakNamedPropertyDeleter) {
-  UNWRAP
-  info.GetReturnValue().Set(!dead && Nan::Delete(obj, property).FromJust());
-}
-
-
-NAN_INDEX_GETTER(WeakIndexedPropertyGetter) {
-  UNWRAP
-  info.GetReturnValue().Set(dead ? Local<Value>() : Nan::Get(obj, index).ToLocalChecked());
-}
-
-
-NAN_INDEX_SETTER(WeakIndexedPropertySetter) {
-  UNWRAP
-  if (!dead) Nan::Set(obj, index, value);
-  info.GetReturnValue().Set(value);
-}
-
-
-NAN_INDEX_QUERY(WeakIndexedPropertyQuery) {
-  info.GetReturnValue().Set(None);
-}
-
-
-NAN_INDEX_DELETER(WeakIndexedPropertyDeleter) {
-  UNWRAP
-  info.GetReturnValue().Set(!dead && Nan::Delete(obj, index).FromJust());
-}
-
-
 /**
- * Only one "enumerator" function needs to be defined. This function is used for
- * both the property and indexed enumerator functions.
- */
-
-NAN_PROPERTY_ENUMERATOR(WeakPropertyEnumerator) {
-  UNWRAP
-  info.GetReturnValue().Set(dead ? Nan::New<Array>(0) : Nan::GetPropertyNames(obj).ToLocalChecked());
-}
-
-/**
- * Weakref callback function. Invokes the "global" callback function,
- * which emits the _CB event on the per-object EventEmitter.
+ * Weakref callback function. Invokes the "global" callback function.
  */
 
 static void TargetCallback(const Nan::WeakCallbackInfo<proxy_container> &info) {
   Nan::HandleScope scope;
   proxy_container *cont = info.GetParameter();
 
-  // invoke global callback function
-  Local<Value> argv[] = {
-    Nan::New<Object>(cont->emitter)
-  };
-  // Invoke callback directly, not via Nan::Callback->Call() which uses
-  // node::MakeCallback() which calls into process._tickCallback()
-  // too. Those other callbacks are not safe to run from here.
-  v8::Local<v8::Function> globalCallbackDirect = globalCallback->GetFunction();
-  globalCallbackDirect->Call(Nan::GetCurrentContext()->Global(), 1, argv);
-
   // clean everything up
   Local<Object> proxy = Nan::New<Object>(cont->proxy);
   Nan::SetInternalFieldPointer(proxy, 0, NULL);
   cont->proxy.Reset();
-  cont->emitter.Reset();
   delete cont;
 }
 
 /**
- * `_create(obj, emitter)` JS function.
+ * `_create(obj)` JS function.
  */
 
 NAN_METHOD(Create) {
@@ -164,10 +83,8 @@ NAN_METHOD(Create) {
   proxy_container *cont = new proxy_container();
 
   Local<Object> _target = info[0].As<Object>();
-  Local<Object> _emitter = info[1].As<Object>();
   Local<Object> proxy = Nan::New<ObjectTemplate>(proxyClass)->NewInstance();
   cont->proxy.Reset(proxy);
-  cont->emitter.Reset(_emitter);
   cont->target.Reset(_target);
   Nan::SetInternalFieldPointer(proxy, 0, cont);
 
@@ -234,23 +151,7 @@ NAN_METHOD(IsDead) {
   info.GetReturnValue().Set(IsDead(proxy));
 }
 
-/**
- * `_getEmitter(weakref)` JS function.
- */
 
-NAN_METHOD(GetEmitter) {
-  WEAKREF_FIRST_ARG
-  info.GetReturnValue().Set(GetEmitter(proxy));
-}
-
-/**
- * Sets the global weak callback function.
- */
-
-NAN_METHOD(SetCallback) {
-  Local<Function> callbackHandle = info[0].As<Function>();
-  globalCallback = new Nan::Callback(callbackHandle);
-}
 
 /**
  * Init function.
@@ -280,8 +181,6 @@ NAN_MODULE_INIT(Initialize) {
   Nan::SetMethod(target, "isNearDeath", IsNearDeath);
   Nan::SetMethod(target, "isDead", IsDead);
   Nan::SetMethod(target, "_create", Create);
-  Nan::SetMethod(target, "_getEmitter", GetEmitter);
-  Nan::SetMethod(target, "_setCallback", SetCallback);
 }
 
 } // anonymous namespace
